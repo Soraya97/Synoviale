8408ea6b4f9f1e0ac176e1db65ab5c667319fc07
AdriWard <AdriWard>
2020-06-30 09:17:29 +0200

    ajout de migration,seeder, model et controleur

---
+++ Laravel/app/Http/Controllers/TestDayController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TestDay;
+use Illuminate\Http\Request;
+
+class TestDayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TestDay  $testDay
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TestDay $testDay)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TestDay  $testDay
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TestDay $testDay)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TestDay  $testDay
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TestDay $testDay)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TestDay  $testDay
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TestDay $testDay)
+    {
+        //
+    }
+}
---
+++ Laravel/database/seeds/JobSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class JobSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
