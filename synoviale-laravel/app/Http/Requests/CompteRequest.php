b1118a1d4b594f3865fd819356cb5917af2df12f
Soraya97 <soraya.meystre@heig-vd.ch>
2020-07-07 09:15:16 +0200

    CompteRequest modified

--- synoviale-laravel/app/Http/Requests/CompteRequest.php
+++ synoviale-laravel/app/Http/Requests/CompteRequest.php
@@ -24,41 +24,32 @@ class CompteRequest extends FormRequest
     public function rules()
     {
         return [
-          // 'name' => 'required |min:3|max:100|regex:([A-Z]{1})',
-          // 'firstname' => 'required|min:3|max:100|regex:([A-Z]{1})',
-          //
-          // 'ID' => 'required|numeric', //unique:users
-          // 'password' => 'required|min:6|confirmed',
-          // 'email' => 'required|email|confirmed' //unique:users
+          // user
+          'username' => 'required',
+          'password' => 'required|min:6|confirmed',
+          'email' => 'required|email|confirmed',
 
-          // 'phonenumber' => 'numeric|min:10|max:10|regex:([0-9])',
-          // 'email' => 'email',
-          // 'comment' => 'max:250',
-          // 'street' => 'max:250',
-          // 'streetnumber' => '',
-          // 'pobox' => '',
-          // 'city' => 'regex:([A-Z]{1})',
-          // 'postalcode' => 'min:4|max:4|numeric',
-          // 'country' => 'regex:([A-Z]{1})'
+          // person
+          'name' => 'required |min:3|max:100|regex:([A-Z]{1})',
+          'firstname' => 'required|min:3|max:100|regex:([A-Z]{1})',
+          'email2' => 'required|email|confirmed',
+          'phoneNumber1' => 'numeric|min:10|max:10|regex:([0-9])',
+          'phoneNumber2' => 'numeric|min:10|max:10|regex:([0-9])',
+          'comment' => 'max:250',
 
-          // 'phonenumber' => 'numeric|min:10|max:10|regex:([0-9])',
-          // 'email' => 'email',
-          // 'comment' => 'max:250',
-          // 'street' => 'max:250',
-          // 'streetnumber' => '',
-          // 'pobox' => '',
-          // 'city' => 'regex:([A-Z]{1})',
-          // 'postalcode' => 'min:4|max:4|numeric',
-          // 'country' => 'regex:([A-Z]{1})'
-          //
-          // 'number' => 'required|min:1|unique',
-          //
-          // 'phoneNumber1' => 'null',
-          // 'phoneNumber2' => 'null',
-          // 'email1' => 'email:rfc,dns|null',
-          // 'email2' => 'email:rfc,dns|null',
-          // 'comment' => 'null|min:3',
-          // 'address_id' => 'required',
+          // address
+          'street1' => 'max:250',
+          'streetNumber' => '',
+          'street2' => 'max:250',
+          'POBox' => '',
+
+          // city
+          'city' => 'regex:([A-Z]{1})',
+          'postalCode' => 'min:4|max:4|numeric',
+
+          // country
+          'country' => 'regex:([A-Z]{1})'
+
         ];
     }
 }
