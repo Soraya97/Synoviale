fa22716e6bba00206a3b143152a75b5bf241df7b
Audrey Zeugin <audreyzeugin@gmail.com>
2020-07-08 11:08:55 +0200

    petits changments

--- synoviale-laravel/database/seeds/BrandSeeder.php
+++ synoviale-laravel/database/seeds/BrandSeeder.php
@@ -17,25 +17,25 @@ class BrandSeeder extends Seeder
         DB::table('brands')->insert([[
            'name' => 'Yeti',
             'shortDescr' => 'Designed to Race. Built to Rally.',
-            'longDescr' => 'Très bonne marque',
+            'longDescr' => 'Très bonne marque, vélo de qualité',
             'company_id' => '1',
         ],
                 [
            'name' => 'Orange',
             'shortDescr' => 'The finiest bicycle product',
-            'longDescr' => 'Très bonne marque',
+            'longDescr' => 'Très bonne marque, vélo tout terrain très maniable',
             'company_id' => '1',
         ],
                 [
            'name' => 'Moustache',
             'shortDescr' => 'Pour laisser la voiture au garage...',
-            'longDescr' => 'Très bonne marque',
+            'longDescr' => 'Très bonne marque, vélo avec une très bonne suspension',
             'company_id' => '1',
         ],
                 [
            'name' => 'Scott',
             'shortDescr' => 'Heroes inspire heroes',
-            'longDescr' => 'Très bonne marque',
+            'longDescr' => 'Très bonne marque, vélo fait avec des amtériaux de qualité',
             'company_id' => '1',
         ]]);
     }
